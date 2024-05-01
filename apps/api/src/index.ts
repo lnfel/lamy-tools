537f2d54c0597f2e8f5d26247621b552d6602ffd
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-05-01 11:14:54 +0800

    [Feature] API using ElysiaJS

---
+++ apps/api/src/index.ts
@@ -0,0 +1,20 @@
+import { Elysia } from "elysia"
+import sharp from "sharp"
+
+const app = new Elysia()
+    .get("/", async ({ set }) => {
+        const image = Bun.file('suisei_august_2023.webp')
+        // const imageSharp = await sharp(await image.arrayBuffer())
+        //     .webp()
+        //     .toBuffer()
+        set.headers = {
+            "Content-Type": image.type
+        }
+        // return imageSharp
+        return new File([await image.arrayBuffer()], `some-image.${image.type.replace('image/', '')}`)
+    })
+    .listen(3000)
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
