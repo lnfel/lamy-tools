97156b03b40ab9cc58d425f04880faaba8477d68
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-05-04 13:38:11 +0800

    [Change] Test endpoint for Image response and upload

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,51 @@
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
+import { cors } from '@elysiajs/cors'
 import sharp from "sharp"
 
-const app = new Elysia()
+export const app = new Elysia()
+    .use(cors())
+    .get("/test", () => "Hi")
     .get("/", async ({ set }) => {
         const image = Bun.file('suisei_august_2023.webp')
         // const imageSharp = await sharp(await image.arrayBuffer())
         //     .webp()
         //     .toBuffer()
         set.headers = {
+            // "Accept-Ranges": "bytes",
+            "Content-Disposition": `filename="${image.name}"`,
+            // "Content-Length": image.size.toString(),
+            // "Content-Range": `0-${image.size - 1}/${image.size}`,
             "Content-Type": image.type
+            // "Content-Type": 'application/octet-stream'
         }
+        // set.headers = {
+        //     "Content-Type": image.type
+        // }
         // return imageSharp
-        return new File([await image.arrayBuffer()], `some-image.${image.type.replace('image/', '')}`)
+        // return new File([await image.arrayBuffer()], `some-image.${image.type.replace('image/', '')}`)
+        return new Blob([await image.arrayBuffer()], { type: image.type })
+        // return await image.arrayBuffer()
+        // return image
+    }, {
+        // response: {
+        //     200: t.File()
+        // }
+    })
+    .post('/upload', async ({ body, set }) => {
+        console.log({ body })
+        // set.headers = {
+        //     "Content-Disposition": `filename="${body.image.name}"`,
+        //     "Content-Type": body.image.type
+        // }
+        const formData = new FormData()
+        formData.append('image', body.image)
+        // return new Blob([await body.image.arrayBuffer()], { type: body.image.type })
+        // return body.image
+        return formData
+    }, {
+        body: t.Object({
+            image: t.File()
+        })
     })
     .listen(3000)
 
