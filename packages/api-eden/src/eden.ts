831d9005e8ee319eff7a210a84d3a6ec98cf8921
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-05-05 20:04:47 +0800

    [Feature] LamyAPI class constructor

--- packages/api-eden/src/eden.ts
+++ packages/api-eden/src/eden.ts
@@ -2,6 +2,26 @@ import { edenTreaty, edenFetch, treaty } from '@elysiajs/eden'
 // import { app } from 'api'
 import type { App } from 'api'
 
+export class LamyAPI {
+    domain
+
+    constructor(domain = 'http://0.0.0.0:3000/') {
+        this.domain = domain
+    }
+
+    edenTreaty() {
+        return edenTreaty<App>(this.domain)
+    }
+
+    treaty() {
+        return treaty<App>(this.domain)
+    }
+
+    fetch() {
+        return edenFetch<App>(this.domain)
+    }
+}
+
 export const api = edenTreaty<App>('http://0.0.0.0:3000/')
 // export const api = treaty(app)
 // export const api = {}
