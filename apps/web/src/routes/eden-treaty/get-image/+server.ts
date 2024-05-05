bea8bf70a64bd66100bd33462922ea414655dd4d
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-05-05 20:05:57 +0800

    [Change] Remove unnecessary comments

--- apps/web/src/routes/eden-treaty/get-image/+server.ts
+++ apps/web/src/routes/eden-treaty/get-image/+server.ts
@@ -1,46 +1,22 @@
 import { api } from 'api-eden'
 
 export const GET = async ({ setHeaders }) => {
-    const response = await api.index.get({
-        getRaw: true
-    })
     // const { data, response, headers } = await api.index.get({
     //     // headers: {
     //     //     "Accept": "image/*"
     //     // }
-    // })
-    // const result = await fetch('http://localhost:3000')
-    // console.log(await result.blob())
-    // console.log({
-    //     headers,
-    //     data,
-    // })
-    // for (const pair of headers?.entries()) {
-    //     console.log(`${pair[0]}: ${pair[1]}`)
-    // }
-
-    // const uInt8Array = Uint8Array.from(data, c => c.charCodeAt(0))
-    // const blob = new Blob([uInt8Array], { type: headers?.get('content-type') })
-    // console.log({
-    //     uInt8Array,
-    //     blob,
-    // })
-    // return new Response(blob, {
-    //     headers
-    // })
-
-    // const blob = new Blob([data], { type: headers?.get('content-type') })
-    // console.log({
-    //     blob,
-    // })
-    // return new Response(blob, {
-    //     headers
     // })
 
     // return new Response(data, {
     //     headers
     // })
 
+    /**
+     * EdenTreaty (Legacy)
+     */
+    const response = await api.index.get({
+        getRaw: true
+    })
     return response as unknown as Response
 }
 
