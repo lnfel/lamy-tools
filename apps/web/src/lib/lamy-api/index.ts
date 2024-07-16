17c6d1cb86bcea197c57137e3d1d06b3cb952d4a
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-07-16 13:07:43 +0800

    [Bugfix] Annotate erronous type, fixes issue with The inferred type of X cannot be named without a reference to Y

--- apps/web/src/lib/lamy-api/index.ts
+++ apps/web/src/lib/lamy-api/index.ts
@@ -1,15 +1,16 @@
-import { LamyAPI, treaty } from "api-eden"
+import type {} from "@elysiajs/eden"
+import { LamyAPI } from "api-eden"
 import config from "$lib/config"
 
 /**
- * [TS Bug SOLVED]: Set "stripInternal": true in tsconfig compilerOptions
- * https://github.com/typescript-eslint/typescript-eslint/issues/7605#issuecomment-1732471255
+ * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1270716220
+ * https://github.com/quadristan/ts-indirect-type-reference-bug
  * 
- * Fixes https://github.com/microsoft/TypeScript/issues/47663
- * 
- * @internal
+ * One of the solution is to type annotate the thing:
+ * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
  */
-export const api = new LamyAPI(config.lamy_api.baseURL)
+export const api: ReturnType<typeof LamyAPI.prototype.edenTreaty> = new LamyAPI(config.lamy_api.baseURL)
     .edenTreaty()
-export const apiTreaty = new LamyAPI(config.lamy_api.baseURL)
+
+export const treaty = new LamyAPI(config.lamy_api.baseURL)
     .treaty()
--- packages/api-eden/src/eden.ts
+++ packages/api-eden/src/eden.ts
@@ -5,8 +5,10 @@
  * ```
  * Possibly due to private class property named dependencies cannot be compared as equal between two separate class instances.
  * @see {@link https://medium.com/@tobygstimpson/typescript-classes-access-modifiers-50900176dd57 | TypeScript Classes — Access Modifiers}
+ * 
+ * Fixed by re-installing api workspace via `pnpm add api`
  */
-import { edenTreaty, edenFetch, treaty as treaty2 } from 'api/node_modules/@elysiajs/eden'
+import { edenTreaty, edenFetch, treaty as treaty2 } from '@elysiajs/eden'
 import type { App } from 'api'
 
 export class LamyAPI {
@@ -17,14 +19,14 @@ export class LamyAPI {
     }
 
     /**
-     * [TS Bug SOLVED]: Set "stripInternal": true in tsconfig compilerOptions
-     * https://github.com/typescript-eslint/typescript-eslint/issues/7605#issuecomment-1732471255
-     * 
-     * Fixes https://github.com/microsoft/TypeScript/issues/47663
-     * 
-     * @internal
+     * Error:
+     * ```md
+     * > The inferred type of 'edenTreaty' cannot be named without a reference to '../node_modules/@elysiajs/eden/dist/types-DcP_1Cpm'. This is likely not portable. A type annotation is necessary.
+     * ```
+     * One of the solution is to type annotate the thing:
+     * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
      */
-    edenTreaty() {
+    edenTreaty(): ReturnType<typeof edenTreaty<App>> {
         return edenTreaty<App>(this.domain)
     }
 
@@ -49,8 +51,11 @@ export class LamyAPI {
      * Fixes https://github.com/microsoft/TypeScript/issues/47663
      * 
      * @internal
+     * 
+     * One of the solution is to type annotate the thing:
+     * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
      */
-    fetch() {
+    fetch(): ReturnType<typeof edenFetch<App>> {
         return edenFetch<App>(this.domain)
     }
 }
@@ -62,8 +67,11 @@ export class LamyAPI {
  * Fixes https://github.com/microsoft/TypeScript/issues/47663
  * 
  * @internal
+ * 
+ * One of the solution is to type annotate the thing:
+ * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
  */
-export const api = edenTreaty<App>('http://localhost:3000/')
+export const api: ReturnType<typeof edenTreaty<App>> = edenTreaty<App>('http://localhost:3000/')
 export const treaty = treaty2<App>('localhost:3000')
 /**
  * [TS Bug SOLVED]: Set "stripInternal": true in tsconfig compilerOptions
@@ -72,5 +80,8 @@ export const treaty = treaty2<App>('localhost:3000')
  * Fixes https://github.com/microsoft/TypeScript/issues/47663
  * 
  * @internal
+ * 
+ * One of the solution is to type annotate the thing:
+ * https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
  */
-export const fetch = edenFetch<App>('http://localhost:3000/')
+export const fetch: ReturnType<typeof edenFetch<App>> = edenFetch<App>('http://localhost:3000/')
