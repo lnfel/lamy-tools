6b059b4a5d7b34f60e98489bce2374804424bdc2
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-07-16 16:31:38 +0800

    [Bugfix] Specify env in turbo.json file, should fix vercel deployments issues with PUBLIC env

--- apps/web/src/lib/config.js
+++ apps/web/src/lib/config.js
@@ -1,3 +1,13 @@
+/**
+ * Do not forget to add PUBLIC_* and <SOME_PRIVATE_ENV> in turbo.json file
+ * ```
+ * "tasks": {
+ *      "build": {
+ *          "env": ["PUBLIC_*", "SOME_PRIVATE_ENV"]
+ *      }
+ * }
+ * ```
+ */
 import { PUBLIC_ENVIRONMENT, PUBLIC_LAMY_API_PROD_URL } from "$env/static/public"
 
 export default {
@@ -5,6 +15,6 @@ export default {
         baseURL: PUBLIC_ENVIRONMENT.toLowerCase() === 'production'
             ? PUBLIC_LAMY_API_PROD_URL
             : 'http://localhost:3000/'
-        // baseURL: PUBLIC_LAMY_API_PROD_URL ?? 'http://localhost:3000/'
+        // baseURL: process.env?.PUBLIC_LAMY_API_PROD_URL ?? 'http://localhost:3000/'
     }
 }
