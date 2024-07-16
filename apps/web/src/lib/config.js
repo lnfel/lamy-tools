0797eb3910f8045d52857b1252d8b72f48a81e03
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-07-16 15:39:15 +0800

    [Change] Test bring back PUBLIC env

--- apps/web/src/lib/config.js
+++ apps/web/src/lib/config.js
@@ -1,10 +1,10 @@
-import { PUBLIC_LAMY_API_PROD_URL } from "$env/static/public"
+import { PUBLIC_ENVIRONMENT, PUBLIC_LAMY_API_PROD_URL } from "$env/static/public"
 
 export default {
     lamy_api: {
-        // baseURL: ENVIRONMENT.toLowerCase() === 'production'
-        //     ? LAMY_API_PROD_URL
-        //     : 'http://localhost:3000/'
-        baseURL: PUBLIC_LAMY_API_PROD_URL ?? 'http://localhost:3000/'
+        baseURL: PUBLIC_ENVIRONMENT.toLowerCase() === 'production'
+            ? PUBLIC_LAMY_API_PROD_URL
+            : 'http://localhost:3000/'
+        // baseURL: PUBLIC_LAMY_API_PROD_URL ?? 'http://localhost:3000/'
     }
 }
